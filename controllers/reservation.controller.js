0777729ffdba2bff80975fd2eb2d8c736e9394ce
MateoRivera <62917220+MateoRivera@users.noreply.github.com>
2022-11-14 20:08:58 -0500

    Servicios de reservación terminados

--- controllers/reservation.controller.js
+++ controllers/reservation.controller.js
@@ -26,57 +26,66 @@ const getReservation = async (req, res) => {
 
 const setReservation = async (req, res) => {
     //Se consulta en la base de datos
-    await prisma.user.create({
+    await prisma.reservation.create({
         data: {
-            ...req.body
+            nameMovie: req.body.nameMovie,
+            date: new Date(req.body.date),
+            hourStart: req.body.hourStart,
+            amountChairs: req.body.amountChairs,
+            User: {
+                connect: {
+                    id: req.body.userId
+                }
+            }
         },
-    }).then((user) => {
-        //Se envia la respuesta
-        res.status(200).json({
-            message: 'Usuario creado',
-            user
-        });
-    }).catch((error) => {
-        res.status(500).json({message: 'No se pudo crear el usuario', error})
+        
+    }).then((reservation) => {
+            res.status(200).json({
+                message: 'Reservación creada',
+                reservation
+            });
+    }).catch((error) => {        
+            res.status(500).json({message: 'Este usuario no existe o faltan datos', error});
     });
 };
 
 const updateReservation = async (req, res) => {
     //Se consulta en la base de datos
-    //res.json({params: req.params, body: req.body});
-    const userUpdated = await prisma.user.update({
+    if(req.body.date){
+        req.body.date = new Date(req.body.date);
+    }
+    await prisma.reservation.update({
         where: {
-            id: req.params.userId,
+            id: req.params.reservationId,
         },
         data: {
             ...req.body
         },
-    }).then((user) => {
+    }).then((reservation) => {
         res.status(200).json({
-            message: 'Usuario actualizado',
-            user
+            message: 'Reservación actualizado',
+            reservation
         });
     }).catch((error) => {
-        res.status(404).send({message: 'Usuario no encontrado'});
+        res.status(404).send({message: 'Reservación no encontrada', error});
     });
     
 };
 
 const deleteReservation = async (req, res) => {
     //Se consulta en la base de datos
-    await prisma.user.delete({
+    await prisma.reservation.delete({
         where: {
-            id: req.params.userId,
+            id: req.params.reservationId,
         },
-    }).then((user) => {
-        //Se envia la respuesta
+    }).then((reservation) => {
         res.status(200).json({
-            message: 'Usuario eliminado',
-            user
+            message: 'Reservación eliminada',
+            reservation
         });
     }).catch((error) => {
-        res.status(404).send({message: 'Usuario no encontrado'});
-    });
+        res.status(404).send({message: 'Reservación no encontrada'});
+    });    
 };
 
 
